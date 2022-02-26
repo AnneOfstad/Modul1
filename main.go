0b3bd448f3f8faa6cf00d92e1619d9720642ec34
AnneOfstad <89352878+AnneOfstad@users.noreply.github.com>
2022-02-26 10:55:31 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,13 @@
+package myqoute
+
+import (
+	"fmt"
+	"github.com/AnneOfstad/Modul1/myquote"
+)
+
+func main() {
+	fmt.Print(myquote.Hello())
+	fmt.Print(myquote.Glass())
+	fmt.Print(myquote.Go())
+	fmt.Print(myquote.Opt())
+}
