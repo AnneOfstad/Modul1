27b1ddeb7a0b7076d70f5836fc80281217d9f4ab
AnneOfstad <89352878+AnneOfstad@users.noreply.github.com>
2022-02-28 10:00:31 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/AnneOfstad/Modul1/myquote"
+	
+
 )
 
 func main() {
-	fmt.Println(myquote.GetHello())
+	fmt.Println(myquote.Hello())
+	fmt.Println(myquote.Glass())
+	fmt.Println(myquote.Go())
+	fmt.Println(myquote.Opt())
 }
